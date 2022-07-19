4e369412a4e19a65f6ea14303b4c54b2002154c1
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-19 20:00:51 +0300

    Added small changes

--- src/components/BlanketSize.js
+++ src/components/BlanketSize.js
@@ -13,27 +13,32 @@ import { useState } from "react";
 import blanketIMG from "../img/blanket1.jpg";
 //Import formulas for our calculating functions
 import { calculateBlanketRows, calculateBlanketStitches } from "../formulas";
+//Import constants
 import {
-  initialStitches,
-  initialRows,
-  initialWidth,
-  initialLength,
-  initialBlanketWidth,
-  initialBlanketLength,
-  initialTotalRows,
-  iniatialTotalStitches,
+  INITIAL_STITCHES,
+  INITIAL_ROWS,
+  INITIAL_WIDTH,
+  INITIAL_LENGTH,
+  INITIAL_BLANKET_WIDTH,
+  INITIAL_BLANKET_LENGTH,
+  INITIAL_TOTAL_ROWS,
+  INITIAL_TOTAL_STITCHES,
 } from "../constants";
+
+//Import animations
+import { popup, imageAnimation, whileHover } from "../animations";
+import { motion } from "framer-motion";
 
 const BlanketSize = () => {
   //State
-  const [stitches, setStitches] = useState(0);
-  const [rows, setRows] = useState(0);
-  const [width, setWidth] = useState(0);
-  const [length, setLength] = useState(0);
-  const [desiredWidth, setDesiredWidth] = useState(0);
-  const [desiredLength, setDesiredLength] = useState(0);
-  const [totalStitches, setTotalStitches] = useState(0);
-  const [totalRows, setTotalRows] = useState(0);
+  const [stitches, setStitches] = useState(INITIAL_STITCHES);
+  const [rows, setRows] = useState(INITIAL_ROWS);
+  const [width, setWidth] = useState(INITIAL_WIDTH);
+  const [length, setLength] = useState(INITIAL_LENGTH);
+  const [desiredWidth, setDesiredWidth] = useState(INITIAL_BLANKET_WIDTH);
+  const [desiredLength, setDesiredLength] = useState(INITIAL_BLANKET_LENGTH);
+  const [totalStitches, setTotalStitches] = useState(INITIAL_TOTAL_STITCHES);
+  const [totalRows, setTotalRows] = useState(INITIAL_TOTAL_ROWS);
 
   //Calc functions
   //Calculate Stitches
@@ -76,143 +81,160 @@ const BlanketSize = () => {
 
   //Cleared button
   const resetCalc = () => {
-    setStitches(initialStitches);
-    setRows(initialRows);
-    setWidth(initialWidth);
-    setLength(initialLength);
-    setDesiredLength(initialBlanketLength);
-    setDesiredWidth(initialBlanketWidth);
-    setTotalRows(initialTotalRows);
-    setTotalStitches(iniatialTotalStitches);
+    setStitches(INITIAL_STITCHES);
+    setRows(INITIAL_ROWS);
+    setWidth(INITIAL_WIDTH);
+    setLength(INITIAL_LENGTH);
+    setDesiredLength(INITIAL_BLANKET_LENGTH);
+    setDesiredWidth(INITIAL_BLANKET_WIDTH);
+    setTotalRows(INITIAL_TOTAL_ROWS);
+    setTotalStitches(INITIAL_TOTAL_STITCHES);
   };
 
   return (
-    <Container>
-      <Card>
-        <Row>
-          <Col>
-            <Card className="text-center">
-              <div className="overflow">
-                <img src={blanketIMG} alt="" />
-              </div>
-            </Card>
-          </Col>
-          <Col>
-            <Card.Body>
-              <Card.Title> Blanket Size Calculator</Card.Title>
-              <Card.Text>
-                Fill some info to get a total blanket size.{" "}
-                <span>
-                  But first, You should measure your knitted gauge in cm
-                </span>
-              </Card.Text>
-              <div className="container">
-                <div className="row">
-                  <div className="col-md-12 d-flex justify-content-end">
-                    <Button
-                      onClick={resetCalc}
-                      type="button"
-                      className="btn btn-secondary"
-                    >
-                      Clear
-                    </Button>
+    <motion.div variants={popup} initial="hidden" animate="show">
+      <Container>
+        <Card>
+          <Row>
+            <Col>
+              <Card className="text-center">
+                <div className="overflow">
+                  <motion.img
+                    variants={imageAnimation}
+                    initial="hidden"
+                    animate="show"
+                    src={blanketIMG}
+                    alt="blanket_image"
+                  />
+                </div>
+              </Card>
+            </Col>
+            <Col>
+              <Card.Body>
+                <Card.Title>
+                  {" "}
+                  <h1>Blanket Size Calculator</h1>
+                </Card.Title>
+                <Card.Text>
+                  Fill some info to get a total blanket size.{" "}
+                  <span>
+                    But first, You should measure your knitted gauge in cm
+                  </span>
+                </Card.Text>
+                <div className="container">
+                  <div className="row">
+                    <div className="col-md-12 d-flex justify-content-end">
+                      <Button
+                        onClick={resetCalc}
+                        type="button"
+                        className="btn btn-secondary"
+                      >
+                        Clear
+                      </Button>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <form>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text className="stitches">
-                    Stitches
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={stitches}
-                    onChange={(e) => setStitches(e.target.value)}
-                    aria-label="stitches"
-                  />
-                  <InputGroup.Text className="rows">Rows</InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={rows}
-                    onChange={(e) => setRows(e.target.value)}
-                    aria-label="rows"
-                  />
-                </InputGroup>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text className="width">
-                    Width (cm)
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={width}
-                    onChange={(e) => setWidth(e.target.value)}
-                    aria-label="Width"
-                  />
-                  <InputGroup.Text className="length">
-                    Length (cm){" "}
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={length}
-                    onChange={(e) => setLength(e.target.value)}
-                    aria-label="Length"
-                  />
-                </InputGroup>
-                <p>Desired size of blanket</p>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text className="d-width">
-                    Desired width (cm)
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={desiredWidth}
-                    onChange={(e) => setDesiredWidth(e.target.value)}
-                    aria-label="d-width"
-                  />
-                  <InputGroup.Text className="d-length">
-                    Desired length (cm)
-                  </InputGroup.Text>
-                  <Form.Control
-                    type="number"
-                    min="0"
-                    value={desiredLength}
-                    onChange={(e) => setDesiredLength(e.target.value)}
-                    aria-label="d-length"
-                  />
-                </InputGroup>
-                <div className="text-center">
-                  <Button className="calc" onClick={calculate}>
-                    Calculate
-                  </Button>
-                </div>
-                <p>
-                  The total stitches and rows needed will show below in the
-                  form.
-                </p>
-                <InputGroup className="mb-3">
-                  <InputGroup.Text>Total stitches / rows</InputGroup.Text>
-                  <Form.Control
-                    disabled
-                    aria-label="total-stitches"
-                    value={totalStitches}
-                  />
-                  <Form.Control
-                    disabled
-                    aria-label="total-rows"
-                    value={totalRows}
-                  />
-                </InputGroup>
-              </form>
-            </Card.Body>
-          </Col>
-        </Row>
-      </Card>
-    </Container>
+                <form>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text className="stitches">
+                      Stitches
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={stitches}
+                      onChange={(e) => setStitches(e.target.value)}
+                      aria-label="stitches"
+                    />
+                    <InputGroup.Text className="rows">Rows</InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={rows}
+                      onChange={(e) => setRows(e.target.value)}
+                      aria-label="rows"
+                    />
+                  </InputGroup>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text className="width">
+                      Width (cm)
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={width}
+                      onChange={(e) => setWidth(e.target.value)}
+                      aria-label="Width"
+                    />
+                    <InputGroup.Text className="length">
+                      Length (cm){" "}
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={length}
+                      onChange={(e) => setLength(e.target.value)}
+                      aria-label="Length"
+                    />
+                  </InputGroup>
+                  <p>Desired size of blanket</p>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text className="d-width">
+                      Desired width (cm)
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={desiredWidth}
+                      onChange={(e) => setDesiredWidth(e.target.value)}
+                      aria-label="d-width"
+                    />
+                    <InputGroup.Text className="d-length">
+                      Desired length (cm)
+                    </InputGroup.Text>
+                    <Form.Control
+                      type="number"
+                      min="0"
+                      value={desiredLength}
+                      onChange={(e) => setDesiredLength(e.target.value)}
+                      aria-label="d-length"
+                    />
+                  </InputGroup>
+                  <div className="text-center">
+                    <motion.div
+                      variants={whileHover}
+                      initial="hidden"
+                      animate="show"
+                    >
+                      <Button className="calc" onClick={calculate}>
+                        Calculate
+                      </Button>
+                    </motion.div>
+                  </div>
+                  <p>
+                    The total stitches and rows needed will show below in the
+                    form.
+                  </p>
+                  <InputGroup className="mb-3">
+                    <InputGroup.Text>Total stitches / rows</InputGroup.Text>
+                    <Form.Control
+                      disabled
+                      aria-label="total-stitches"
+                      value={totalStitches}
+                    />
+                    <Form.Control
+                      disabled
+                      aria-label="total-rows"
+                      value={totalRows}
+                    />
+                  </InputGroup>
+                </form>
+              </Card.Body>
+            </Col>
+          </Row>
+        </Card>
+      </Container>
+    </motion.div>
   );
 };
 
--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
-export const initialStitches = 0;
-export const initialRows = 0;
-export const initialWidth = 0;
-export const initialLength = 0;
-export const initialBlanketWidth = 0;
-export const initialBlanketLength = 0;
-export const initialTotalRows = 0;
-export const iniatialTotalStitches = 0;
+export const INITIAL_STITCHES = 0;
+export const INITIAL_ROWS = 0;
+export const INITIAL_WIDTH = 0;
+export const INITIAL_LENGTH = 0;
+export const INITIAL_BLANKET_WIDTH = 0;
+export const INITIAL_BLANKET_LENGTH = 0;
+export const INITIAL_TOTAL_ROWS = 0;
+export const INITIAL_TOTAL_STITCHES = 0;
