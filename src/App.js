36ad5e0168a5a1b3d36c3997cb53c18adeb617a7
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-20 20:25:11 +0300

    Added NavBar and styled a logo a bit.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import BlanketSize from "./components/BlanketSize";
 //Styles
-import "./styles/_app.scss"
+import "./styles/_app.scss";
+import NavBar from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <BlanketSize />
     </div>
-    
-
-
-
-
   );
 }
 
