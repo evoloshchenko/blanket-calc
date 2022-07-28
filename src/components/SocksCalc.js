72f8951199e7a162d5e2405173b8edd345be8bfe
Jane Voloshchenko <j.voloshchenko@gmail.com>
2022-07-28 19:48:35 +0300

    Added forms for Socks Calculator

--- src/components/SocksCalc.js
+++ src/components/SocksCalc.js
@@ -30,7 +30,7 @@ const SocksCalc = () => {
     <div>
       <Container>
         <Card>
-          <Row>
+          <Row className="fill_data">
             <Col>
               <Card className="text-center">
                 <div className="overflow">
@@ -40,15 +40,16 @@ const SocksCalc = () => {
             </Col>
             <Col>
               <Card.Body>
-                <Card.Title>Socks Calculator</Card.Title>
+                <Card.Title>Cuff-Down Socks Calculator</Card.Title>
                 <Card.Text>
                   Fill some info to get a total socks calculation.{" "}
                   <span>
                     {" "}
                     But first you should pick yarn and ornament, knit some gauge
                     - 10*10cm, and than measure your stitches and rows. If your
-                    gauge bigger than standart one, you can use out poles, and
-                    find out your fabric density.
+                    gauge bigger than standart one, you can use our poles, and
+                    find out your fabric density, and use all calculation for
+                    knitting the cuff-down socks.
                   </span>
                 </Card.Text>
                 <div className="container">
@@ -136,13 +137,142 @@ const SocksCalc = () => {
                     <Form.Control
                       type="number"
                       min="0"
-                      value={rows}
+                      // value={rows}
                       //   onChange={(e) => setRows(e.target.value)}
                       aria-label="calfCircum"
                     />
                   </InputGroup>
+                  <div className="text-center">
+                    <Button className="calc">Calculate</Button>
+                  </div>
+                  <div>
+                    <p>
+                      All calculations based on previously filled forms, and
+                      will appear below. We divide it for some parts of sock.
+                    </p>
+                  </div>
                 </Form>
               </Card.Body>
+            </Col>
+          </Row>
+          <Row className="calculated_data">
+            <Col>
+              <div className="text-center">
+                <h4 className="font-weight-bold">The Cuff</h4>
+                <p>
+                  This form will show us the total number of stitches that we
+                  should cast on.
+                </p>
+              </div>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Total Socks Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="total-socks-stitches"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <div className="text-center">
+                <h4 className="font-weight-bold">Heel Flap</h4>
+                <p>Here we can see the info for knitting perfect heel flap</p>
+              </div>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Heel Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="heel-stitches"
+                  // value={totalStitches}
+                />
+                <InputGroup.Text>Gusset Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="gusset-stitches"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Heel Flap Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="heel-flap-stitches"
+                  // value={totalStitches}
+                />
+                <InputGroup.Text>Instep Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="instep-stitches"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <p className="text-center">
+                Number of stitches to pick up for sides of heel flap.
+              </p>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Pick Heel Flap Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="pick-heel-flap-stitches"
+                  // value={totalStitches}
+                />
+                <InputGroup.Text>Heel Flap Length</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="heel-flap-length"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+            </Col>
+            <Col>
+              <div className="text-center">
+                <h4 className="font-weight-bold">Gusset</h4>
+                <p>Number of stitches to decrease extra gusset stitches.</p>
+              </div>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Decrease Gusset Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="decrease-gusset-stitches"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <div className="text-center">
+                <h4 className="font-weight-bold">Toe</h4>
+              </div>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Socks Length</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="socks-length"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <p className="text-center">
+                How much stitches to decrease for making a toe.
+              </p>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Decrease Toe Stitches</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="decrease-toe-stitches"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Toe Rows</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="toe-rows"
+                  // value={totalStitches}
+                />
+              </InputGroup>
+              <InputGroup className="mb-3">
+                <InputGroup.Text>Toe Length</InputGroup.Text>
+                <Form.Control
+                  disabled
+                  aria-label="toe-length"
+                  // value={totalStitches}
+                />
+              </InputGroup>
             </Col>
           </Row>
         </Card>
